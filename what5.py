e39e8b8705432126b5292832e11347e6245f09f1
hunter <hantek.f@gmail.com>
2019-03-10 07:58:37 +0100

    complete product

--- what5.py
+++ what5.py
@@ -91,8 +91,82 @@ with rec.open('blocker.wav', 'wb') as recfile:
 headers = {'Ocp-Apim-Subscription-Key': '6149675feef147f797f24802e78aafac'}
 url = 'https://francecentral.stt.speech.microsoft.com/speech/recognition/dictation/cognitiveservices/v1?cid=48b1fa24-f21f-4b41-8c3f-be319d8b4b26'
 r = requests.post(url, headers=headers, data=open('blocker.wav', 'rb').read())
-
-#print(r.text)
-#print(r.status_code)
 resp = json.loads(r.text)
 print(resp["DisplayText"])
+
+##Playback
+subscription_key = "28024669eb534248bcf6e42329a07558"
+
+if 'SPEECH_SERVICE_KEY' in os.environ:
+    subscription_key = os.environ['SPEECH_SERVICE_KEY']
+elif subscription_key == None:
+    print('Environment variable for your subscription key is not set.')
+    exit()
+
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+
+    def get_token(self):
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        print(response.text)
+        self.access_token = str(response.text)
+
+    def save_audio(self):
+        base_url = 'https://westus.tts.speech.microsoft.com/'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'User-Agent': 'STARTHack'
+        }
+        xml_body = ElementTree.Element('speak', version='1.0')
+        xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+        voice = ElementTree.SubElement(xml_body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'ar-EG')
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)')
+        voice.text = resp["DisplayText"]
+        body = ElementTree.tostring(xml_body)
+
+        response = requests.post(constructed_url, headers=headers, data=body)
+        '''
+        If a success response is returned, then the binary audio is written
+        to file in your working directory. It is prefaced by sample and
+        includes the date.
+        '''
+        if response.status_code == 200:
+            with open('gogogo.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+
+            chunk = 1024
+            f = wave.open(r"gogogo.wav","rb")
+            p = pyaudio.PyAudio()
+            stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+            channels = f.getnchannels(),  
+            rate = f.getframerate(),  
+            output = True)
+            data = f.readframes(chunk) 
+            while data:  
+                stream.write(data)  
+                data = f.readframes(chunk)  
+
+            stream.stop_stream()  
+            stream.close()   
+            p.terminate() 
+        else:
+            print("\nStatus code: " + str(response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
+            print(response.headers)
+
+if __name__ == "__main__":
+    app = TextToSpeech(subscription_key)
+    app.get_token()
+    app.save_audio()
